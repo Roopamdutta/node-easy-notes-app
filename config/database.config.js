dba1210d68be0da390cf6793c7e3ce3ac77d6f3b
Roopam <roopam_dutta@yahoo.com>
2019-11-21 01:19:29 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-   // url: 'mongodb+srv://roopam:123%40qwe%21@india-cluster-osrwz.azure.mongodb.net/test?retryWrites=true&w=majority'
-    url:'mongodb://roopam:123%40qwe%21@india-cluster-shard-00-00-osrwz.azure.mongodb.net:27017,india-cluster-shard-00-01-osrwz.azure.mongodb.net:27017,india-cluster-shard-00-02-osrwz.azure.mongodb.net:27017/test?ssl=true&replicaSet=india-cluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+    url: DB_CONN
 }
