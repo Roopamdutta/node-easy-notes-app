50807dff03e43c8a1d95b1cdfccfaed8e28e62d2
roopam <mr.roopa@yahoo.com>
2019-11-21 01:11:35 +0530

    modified:   config/database.config.js 	modified:   package-lock.json 	modified:   package.json

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: DB_CONN || 'mongodb://localhost:27017/easy-notes';
+   // url: 'mongodb+srv://roopam:123%40qwe%21@india-cluster-osrwz.azure.mongodb.net/test?retryWrites=true&w=majority'
+    url:'mongodb://roopam:123%40qwe%21@india-cluster-shard-00-00-osrwz.azure.mongodb.net:27017,india-cluster-shard-00-01-osrwz.azure.mongodb.net:27017,india-cluster-shard-00-02-osrwz.azure.mongodb.net:27017/test?ssl=true&replicaSet=india-cluster-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
